c1f0d80f3a925a65a427f915a1bde54d88d2ea39
Deanna Gardner <dgardner3695@gmail.com>
2022-01-05 16:04:22 -0500

    added to InformationExistsException class

--- src/main/java/com/example/project2/exception/InformationExistsException.java
+++ src/main/java/com/example/project2/exception/InformationExistsException.java
@@ -1,4 +1,10 @@
 package com.example.project2.exception;
 
-public class InformationExistsException {
-}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InformationExistsException extends RuntimeException {
+    public InformationExistsException(String message){
+        super(message);
+}}
