def149bcf6a960b3edf63b4266e515e81f712ef5
Deanna Gardner <dgardner3695@gmail.com>
2022-01-04 21:07:52 -0500

    CRUD Methods Book Controller

--- src/main/java/com/example/project2/controller/BookController.java
+++ src/main/java/com/example/project2/controller/BookController.java
@@ -1,11 +1,35 @@
 package com.example.project2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BookController {
-    @GetMapping("/book")  //url
-    public String getBook() { return "book"; }
-}
+    @GetMapping(path = "/books/")
+    public String getBooks() {
+        return "get all books";
+    }
+
+    @GetMapping(path = "/books/{booksId}")
+    public String getBook(@PathVariable Long bookId) {
+        return "getting the category with the id of " + bookId;
+
+    }
+
+    @PostMapping("/books/")
+    public String createBook(@RequestBody String body) {
+        return "creating a book " + body;
+    }
+
+    @PutMapping("/books/{bookId}")
+    public String updateBook(@PathVariable(value = "bookId") Long bookId, @RequestBody String body) {
+        return "updating the book with the id of " + bookId + body;
+
+    }
+
+    @DeleteMapping("/books/{booksId}")
+    public String deleteBook(@PathVariable(value = "bookId") Long bookId) {
+        return "deleting the book with the id of " + bookId;
+
+}}
+
 
