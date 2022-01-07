a99fbfaaa94e71301db33a48785cfe3d8239b51d
Deanna Gardner <dgardner3695@gmail.com>
2022-01-07 16:55:42 -0500

    forgot to mention user repository has been updated

--- src/main/java/com/example/project2/repository/UserRepository.java
+++ src/main/java/com/example/project2/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.example.project2.repository;
 
-public interface UserRepository {
+
+import com.example.project2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmailAddress(String userEmailAddress);
+
+    User findUserByEmailAddress(String userEmailAddress);
+
+
 }
