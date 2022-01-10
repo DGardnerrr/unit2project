2d5a32baab2be747bd7f8780f2b7b996f2464fdf
Deanna Gardner <dgardner3695@gmail.com>
2022-01-10 01:52:00 -0500

    comments added

--- src/main/java/com/example/project2/exception/InformationExistsException.java
+++ src/main/java/com/example/project2/exception/InformationExistsException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 //exception model classes
 //handles runtime errors
+//purpose: maintain the normal flow of the application
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistsException extends RuntimeException {
     public InformationExistsException(String message){
