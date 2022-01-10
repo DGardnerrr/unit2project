b6f420971f3f651f5a74c30b71d5e24a3ac83300
Deanna Gardner <dgardner3695@gmail.com>
2022-01-10 01:53:14 -0500

    commited

--- src/main/java/com/example/project2/exception/InformationNotFoundException.java
+++ src/main/java/com/example/project2/exception/InformationNotFoundException.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 //exception model classes
+//handles runtime errors
+//purpose: maintain the normal flow of the application
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException{
     public InformationNotFoundException(String message){
